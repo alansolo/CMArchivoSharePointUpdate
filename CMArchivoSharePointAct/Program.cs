3735a237d33fdef5f5a340f2ab1da779e9ede3aa
Alan Michel Solorzano Tapia <K697344@nac.ppg.com>
2020-10-30 13:43:38 -0600

    Agregar archivos de proyecto.

---
+++ CMArchivoSharePointAct/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SharePoint.Client;
+
+namespace CMArchivoSharePointAct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EjecutarProceso();
+
+        }
+
+        static void EjecutarProceso()
+        {
+
+            ClientContext clientContext = new ClientContext("");
+
+            //Microsoft.SharePoint.Client.ClientObjectList<Object> lista = new ClientObjectList<Object>();
+        }
+    }
+}
---
+++ CMArchivoSharePointUpdate/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace CMArchivoSharePointUpdate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo!");
+
+            EjecutarProceso();
+        }
+
+        static void EjecutarProceso()
+        {
+            
+             
+
+        }
+
+
+    }
+}
